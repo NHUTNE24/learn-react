f390c0f801a3dbc7e0f7ef116af2670d397b84cd
nhutnguyen <nhut.nguyen300@hcmut.edu.vn>
2024-06-13 09:12:05 +0700

    KKK

--- src/App.js
+++ src/App.js
@@ -7,17 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Nguyen Quoc Nhut
+          Hello Nguyen Quoc Nhut (HCMUT)
           Welcome to FSA (FPT Software Academy)
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
